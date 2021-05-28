34109978b0375a95daf7191a28b03fe3f863aabd
mtrevin93 <mtrevin93@gmail.com>
2021-05-28 16:58:46 -0400

    added toy ID creator with a new toy to test

--- leonids-toys.js
+++ leonids-toys.js
@@ -52,12 +52,29 @@ const toyKeyboard = {
 toys.push(actionFigure)
 toys.push(toyKeyboard)
 
+const windUpSoldier = {
+    name: "Wind-Up Soldier",
+    materials: ["plastic", "metal"],
+    color: ["red", "white"],
+    weight: 0.3,
+    price: 3,
+}
+
+const inventoryAddToy = (toy) => {
+    const lastIndex = toys.length -1
+    const currentLastToy = toys[lastIndex]
+    const maxId = currentLastToy.id 
+    const idForNewToy = maxId + 1
+
+    toy.id = idForNewToy
+    toys.push(toy)
+
+}
+inventoryAddToy(windUpSoldier)
 
 // Inflation + ID Search
 for (const toy of toys) {
-    if (toy.id === toyToFind) {
 toy.price = toy.price*1.05
-console.log(`This toy is a ${toy.color} ${toy.name} made out of ${toy.materials}
+console.log(`This toy is a ${toy.color} ${toy.name}, ID#${toy.id} made out of ${toy.materials}
 it costs $${toy.price}`)
-}
 }
